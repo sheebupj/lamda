8e96f8392bddda9466e19222a807495cd622c95b
Sheebu <Sheebu@Paremal>
2020-08-17 17:44:06 +0530

    reduce map-reduce minimum count maximum operations added

--- src/com/paremal/lamda/operations/ProcessEmployee.java
+++ src/com/paremal/lamda/operations/ProcessEmployee.java
@@ -17,13 +17,10 @@ public class ProcessEmployee {
 
 	public static void main(String[] args) {
 		// initialize array of employee
-		Employee[] employees = { new Employee("Jason", "Red", 5000, "IT"),
-				new Employee("Ashly", "Green", 7600, "IT"),
+		Employee[] employees = { new Employee("Jason", "Red", 5000, "IT"), new Employee("Ashly", "Green", 7600, "IT"),
 				new Employee("Mathew", "Indigo", 3587.5, "Sales"),
-				new Employee("James", "Indigo", 4700.77, "Marketing"), 
-				new Employee("Luke", "Indigo", 6200, "IT"),
-				new Employee("Jason", "Blue", 3200, "Sales"),
-				new Employee("Jason", "Blue", 3200, "finance"),
+				new Employee("James", "Indigo", 4700.77, "Marketing"), new Employee("Luke", "Indigo", 6200, "IT"),
+				new Employee("Jason", "Blue", 3200, "Sales"), new Employee("Jason", "Blue", 3200, "finance"),
 				new Employee("Wendy", "Brown", 4236.4, "Marketing") };
 
 		List<Employee> list = Arrays.asList(employees);
@@ -44,7 +41,7 @@ public class ProcessEmployee {
 		 */
 		list.stream().filter(fourToSixThousand).sorted(Comparator.comparing(Employee::getLastName))
 				.forEach(System.out::println);
-		
+
 		System.out.printf("\n\n#highest salary less than 6001 \n%s\n",
 				list.stream().filter(fourToSixThousand).findFirst().get());
 
@@ -53,13 +50,12 @@ public class ProcessEmployee {
 		 */
 		Function<Employee, String> byLastName = Employee::getLastName;
 		Function<Employee, Double> bySalary = Employee::getSalary;
-		
+
 		/*
 		 * Comparator with multiple comparison
 		 */
 		Comparator<Employee> lastThenFirstName = Comparator.comparing(byLastName).thenComparing(bySalary);
 
-		
 		/*
 		 * sort by comparator with multiple value comparing
 		 */
@@ -80,7 +76,8 @@ public class ProcessEmployee {
 		System.out.println("\n\n#going through all item in the list with forEach()");
 		list.stream().forEach(System.out::println);
 		/*
-		 * creating TreeMap with departmentname as key and list of employee as value using Collectores.groupingBY()
+		 * creating TreeMap with departmentname as key and list of employee as value
+		 * using Collectores.groupingBY()
 		 */
 		Map<String, List<Employee>> groupedByDepartMent = list.stream()
 				.collect(Collectors.groupingBy(Employee::getDepartment));
@@ -91,7 +88,8 @@ public class ProcessEmployee {
 		});
 
 		/*
-		 * creating TreeMap with departmentname as key and count as value using Collectores.groupingBY()
+		 * creating TreeMap with departmentname as key and count as value using
+		 * Collectores.groupingBY()
 		 */
 		Map<String, Long> employeeCountByDepartMent = list.stream()
 				.collect(Collectors.groupingBy(Employee::getDepartment, TreeMap::new, Collectors.counting()));
@@ -104,7 +102,7 @@ public class ProcessEmployee {
 		 */
 		System.out.printf("%n%n%n#Sum of the employee salary using sum method %.2f%n",
 				list.stream().mapToDouble(Employee::getSalary).sum());
-		
+
 		/*
 		 * reduce()
 		 */
@@ -116,124 +114,169 @@ public class ProcessEmployee {
 
 		System.out.printf("%n%n%n#Sum of the employee salary using reduce method %.2f%n",
 				list.stream().mapToDouble(Employee::getSalary).reduce(0, (v1, v2) -> v1 + v2));
-		
+
 		/*
-		 * average() getAsDouble() 
+		 * average() getAsDouble()
 		 */
 		System.out.printf("%n%n%n#Average of the employee salary using average method %.2ff%n",
 				list.stream().mapToDouble(Employee::getSalary).average().getAsDouble());
-		
+
 		/*
 		 * getting department name from employee list
 		 */
 		List<String> departmentNames = list.stream().map(Employee::getDepartment).collect(Collectors.toList());
 
-		
-		
 		/*
-		 * getting distinct department names from employee list using stream.map().distict()
+		 * getting distinct department names from employee list using
+		 * stream.map().distict()
 		 */
 		System.out.println("\n\n#Distinct department names");
 		list.stream().map(Employee::getDepartment).distinct().collect(Collectors.toList()).forEach(System.out::println);
-		
+
 		/*
-		 * getting  first names from employee list
+		 * getting first names from employee list
 		 */
 		System.out.println("\n\n#Employee first names");
-		list.stream().map(Employee::getFirstName).forEach(n-> System.out.printf("   %s%n", n));
-		
-		
+		list.stream().map(Employee::getFirstName).forEach(n -> System.out.printf("   %s%n", n));
+
 		/*
 		 * filtering Employees who have more than 5000 salary
 		 */
 		System.out.println("\n\n#Employees who have more than 5000 salary");
-		list.stream().filter(e-> e.getSalary()>5000).forEach(employee -> System.out.printf("   %s%n", employee));
-		
+		list.stream().filter(e -> e.getSalary() > 5000).forEach(employee -> System.out.printf("   %s%n", employee));
+
 		/*
 		 * filtering list for items that have frequency more than one
 		 */
 		System.out.println("\n\n#department names with frequency more than one");
-		departmentNames.stream().filter(i->Collections.frequency(departmentNames, i)>1).collect(Collectors.toList()).forEach(System.out::println);
-			
-		
+		departmentNames.stream().filter(i -> Collections.frequency(departmentNames, i) > 1).collect(Collectors.toList())
+				.forEach(System.out::println);
+
 		/*
 		 * mapping salaries of all employee objects to list of salaries
 		 */
 		System.out.println("\n\n#Salaries");
 		list.stream().map(Employee::getSalary).collect(Collectors.toList()).forEach(System.out::println);
-		
+
 		/*
-		 * filtering Employees  with predicate here e-> e.getSalary()>5000)
+		 * filtering Employees with predicate here e-> e.getSalary()>5000)
 		 */
-		List<Employee> salries=list.stream().filter(e-> e.getSalary()>5000).collect(Collectors.toList());
-		
+		List<Employee> salries = list.stream().filter(e -> e.getSalary() > 5000).collect(Collectors.toList());
+
 		/*
-		 * filtering Employees  with predicate here e-> e.getSalary()>400) chained with skip()
+		 * filtering Employees with predicate here e-> e.getSalary()>400) chained with
+		 * skip()
 		 */
 		System.out.println("\n\n#filtered employee list predicate salary>4000 and skip one");
-		list.stream().filter(e-> e.getSalary()>4000).skip(1).forEach(e -> System.out.printf("   %s%n", e));
-		
+		list.stream().filter(e -> e.getSalary() > 4000).skip(1).forEach(e -> System.out.printf("   %s%n", e));
+
 		/*
 		 * Mapping names of employees to list of strings and print them
 		 */
 		System.out.println("\n\n#employee names only");
 
 		list.stream().map(Employee::getFirstName).forEach(System.out::println);
-		
+
 		/*
 		 * Map() and flatMap()
 		 */
 		List<String> words = Arrays.asList("Hello", "World");
-		
+
 		/*
 		 * following will return Stream<Stream<String>>
 		 */
 		System.out.println("\n\n# trying to get distinct leters from two words using Map()");
-		words.stream().map(word->word.split(""))
-		.map(Arrays::stream).distinct().forEach(System.out::println);
-		
+		words.stream().map(word -> word.split("")).map(Arrays::stream).distinct().forEach(System.out::println);
+
 		/*
-		 * following will return distinct stream<characters> 
+		 * following will return distinct stream<characters>
 		 */
 		System.out.println("\n\n# distinct leters from two words using flatMap()");
-		words.stream().map(word->word.split(""))
-		.flatMap(Arrays::stream).distinct().forEach(System.out::println);
-		
+		words.stream().map(word -> word.split("")).flatMap(Arrays::stream).distinct().forEach(System.out::println);
+
 		/*
 		 * map integer list to its squires
 		 */
 		System.out.println("\n\n# numbers and squares");
-		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5,9);
-		numbers.stream().map(n-> n*n).forEach(System.out::println);
-		
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 9);
+		numbers.stream().map(n -> n * n).forEach(System.out::println);
+
 		/*
 		 * create pairs from two list of integers(combinations)
 		 */
 		System.out.println("\n\n#combination of two integer list");
-		List<Integer> numbers1 = Arrays.asList(1, 2, 3,5,6);
-		List<Integer> numbers2 = Arrays.asList(3, 4,7,9);
-		numbers1.stream().flatMap(i->numbers2.stream()
-				.map(j-> new int[] {i,j}))
-		.forEach(n-> System.out.printf("%d,%d%n", n[0],n[1]));
-		
+		List<Integer> numbers1 = Arrays.asList(1, 2, 3, 5, 6,11);
+		List<Integer> numbers2 = Arrays.asList(3, 4, 7, 9);
+		numbers1.stream().flatMap(i -> numbers2.stream().map(j -> new int[] { i, j }))
+				.forEach(n -> System.out.printf("%d,%d%n", n[0], n[1]));
+
 		/*
 		 * combination of two list of integers, filtering with predicate
 		 */
 		System.out.println("\n\n#after filtering the combination sum less than 7");
-		numbers1.stream().flatMap(i->numbers2.stream()
-				.filter(j-> (i+j)<7).map(j->new int[] {i,j}))
-		.forEach(n->System.out.printf("%d,%d%n",n[0],n[1]));
-		
+		numbers1.stream().flatMap(i -> numbers2.stream().filter(j -> (i + j) < 7).map(j -> new int[] { i, j }))
+				.forEach(n -> System.out.printf("%d,%d%n", n[0], n[1]));
+
 		/*
 		 * combination of two list of integers, filtering with predicate sum%5==5
 		 */
 		System.out.println("\n\n#after filtering the combination with predicate  sum%5==0");
-		List<int[]> pairs8=numbers1.stream()
-				.flatMap(i->numbers2.stream()
-						.filter(j-> (i+j)%5==0)
-						.map(j-> new int[] {i,j}))
+		List<int[]> pairs8 = numbers1.stream()
+				.flatMap(i -> numbers2.stream().filter(j -> (i + j) % 5 == 0).map(j -> new int[] { i, j }))
 				.collect(Collectors.toList());
-		pairs8.forEach(n->System.out.printf("%d,%d%n",n[0],n[1]));
+		pairs8.forEach(n -> System.out.printf("%d,%d%n", n[0], n[1]));
+
+		/*
+		 * find first with predicate
+		 */
+		System.out.println("first no matching the predicate");
+		numbers1.stream().filter(n -> n < 9 && n % 5 == 0).findFirst().ifPresent(System.out::println);
+
+		/*
+		 * calculate sum of the list with reduce()
+		 */
+
+		System.out.println("Sum of all numbers in the list:" + numbers1.stream().reduce(0, (a, b) -> a + b));
+
+		/*
+		 * sum of integer list using sum method in Integer class
+		 * 
+		 */
+		System.out.println("Sum of all numbers in the list using sum method in Integer class:"
+				+ numbers1.stream().reduce(Integer::sum));
+		/*
+		 * minimum using reduce
+		 */
+		System.out.println("Minimum of  numbers in the list using sum  method in Integer class and reduce:"
+				+ numbers1.stream().reduce( Integer::min));
 		
+		/*
+		 * Maximum using reduce
+		 */
+		System.out.println("Maximum of  numbers in the list using sum  method in Integer class and reduce:"
+				+ numbers1.stream().reduce( Integer::max));
+		
+		/*
+		 * Maximum without using reduce
+		 */
+		System.out.println("Maximum of  numbers in the list using sum  method in Integer class and without reduce:"
+				+ numbers1.stream().reduce(( a,b)-> a>b?a:b));
+		/*
+		 * Minimum without using reduce
+		 */
+		System.out.println("Minimum of  numbers in the list using sum  method in Integer class and without reduce:"
+				+ numbers1.stream().reduce(( a,b)-> a<b?a:b));
+		/*
+		 * count of  numbers in the list using map-reduce method
+		 */
+		System.out.println("count of  numbers in the list using map-reduce method:"
+				+ numbers1.stream().map(n-> 1).reduce(( a,b)-> a+b));/*
+		 * count of  numbers in the list using count method
+		 */
+		System.out.println("count of  numbers in the list using count method:"
+				+ numbers1.stream().count());
+		
+		
+
 	}
 }
