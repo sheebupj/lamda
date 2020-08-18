42caaa157a73036d0adf7b03409d08d8b04eb3a0
Sheebu <Sheebu@Paremal>
2020-08-18 09:38:03 +0530

    formatted

--- src/com/paremal/lamda/operations/Trade.java
+++ src/com/paremal/lamda/operations/Trade.java
@@ -8,52 +8,41 @@ import java.util.List;
 public class Trade {
 
 	public static void main(String[] args) {
-		
+
 		Trader raoul = new Trader("Raoul", "Cambridge");
 		Trader mario = new Trader("Mario", "Milan");
 		Trader alan = new Trader("Alan", "Cambridge");
 		Trader brian = new Trader("Brian", "Cambridge");
-		List<Trader> traders=new ArrayList<Trader>();
+		List<Trader> traders = new ArrayList<Trader>();
 		traders.add(raoul);
 		traders.add(mario);
 		traders.add(alan);
 		traders.add(brian);
-		
-		List<Transaction> transactions =
-				Arrays.asList(
-				new Transaction(brian, 2011, 300),
-				new Transaction(raoul, 2012, 1000),
-				new Transaction(raoul, 2011, 400),
-				new Transaction(mario, 2012, 710), 
-				new Transaction(mario, 2012, 700),
-				new Transaction(alan, 2012, 950)
-				);
-		
+
+		List<Transaction> transactions = Arrays.asList(new Transaction(brian, 2011, 300),
+				new Transaction(raoul, 2012, 1000), new Transaction(raoul, 2011, 400),
+				new Transaction(mario, 2012, 710), new Transaction(mario, 2012, 700), new Transaction(alan, 2012, 950));
+
 		/*
-		 * Find all transactions in the year 2011 and sort them by value (small to high) from list<Transactions>
+		 * Find all transactions in the year 2011 and sort them by value (small to high)
+		 * from list<Transactions>
 		 */
 		System.out.println("Find all transactions in the year 2011 and sort them by value (small to high)");
-		transactions.stream()
-		.filter(t-> t.getYear()==2011)
-		.sorted(Comparator.comparing(Transaction::getValue))
-		.forEach(tr->System.out.println(tr));
-		
+		transactions.stream().filter(t -> t.getYear() == 2011).sorted(Comparator.comparing(Transaction::getValue))
+				.forEach(tr -> System.out.println(tr));
+
 		/*
 		 * all the unique cities where the traders work from list<Transactions>
 		 */
 		System.out.println("\n\nall the unique cities where the traders work");
-		transactions.stream().map(t->t.getTrader().getCity()).distinct().forEach(System.out::println);
-		
-		
+		transactions.stream().map(t -> t.getTrader().getCity()).distinct().forEach(System.out::println);
+
 		/*
 		 * Find all traders from Cambridge and sort them by name from list<Transactions>
 		 */
 		System.out.println("\n\nFind all traders from Cambridge and sort them by name");
-		transactions.stream()
-		.filter(t->t.getTrader().getCity().equals("Cambridge"))
-		.map(t-> t.getTrader().getName())
-		.sorted()
-		.forEach(System.out::println);
+		transactions.stream().filter(t -> t.getTrader().getCity().equals("Cambridge")).map(t -> t.getTrader().getName())
+				.sorted().forEach(System.out::println);
 	}
 
 }
