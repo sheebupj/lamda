57734d6e2186352ea9d75b146efca00e374b0eaa
Sheebu <Sheebu@Paremal>
2020-08-18 14:18:38 +0530

    generating Pythagorean triple numbers

--- src/com/paremal/lamda/operations/PrimitivStreamOperations.java
+++ src/com/paremal/lamda/operations/PrimitivStreamOperations.java
@@ -2,10 +2,11 @@ package com.paremal.lamda.operations;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class PrimitivStreamOperations {
 	public static void main(String[] args) {
-		
+
 		Trader raoul = new Trader("Raoul", "Cambridge");
 		Trader mario = new Trader("Mario", "Milan");
 		Trader alan = new Trader("Alan", "Cambridge");
@@ -14,12 +15,28 @@ public class PrimitivStreamOperations {
 		List<Transaction> transactions = Arrays.asList(new Transaction(brian, 2011, 300),
 				new Transaction(raoul, 2012, 1000), new Transaction(raoul, 2011, 400),
 				new Transaction(mario, 2012, 710), new Transaction(mario, 2012, 700), new Transaction(alan, 2012, 950));
-		
+
 		/*
 		 * convert Integer stream to intStram only primitive stream define sum method
 		 */
-		System.out.println( "sum of transaction values"+ transactions.stream().mapToInt(Transaction::getValue).sum());
-		
+		System.out.println("sum of transaction values" + transactions.stream().mapToInt(Transaction::getValue).sum());
+
+		/*
+		 * generating a range of values using intStream 1-100 even numbers
+		 */
+		System.out.println("1-100 even numbers");
+		IntStream.rangeClosed(1, 100).filter(i -> i % 2 == 0).forEach(n-> System.out.printf("%d ", n));
+
+		/**
+		 * Pythagorean triple
+		 */
+		System.out.println("\nPythagorean triple numbers (a,b)<=100");
+		int thousand=1000;
+		IntStream.rangeClosed(1, thousand).boxed()
+				.flatMap(a -> IntStream.rangeClosed(a, thousand).filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+						.mapToObj(b -> new int[] { a, b,(int) Math.sqrt(a*a + b * b )}))
+				.forEach(o -> System.out.println(o[0] + " " + o[1] + " " + o[2]));
+
 	}
 
 }
