4c241e16cbb3adecc1640ba8313230ec34f31e72
Sheebu <Sheebu@Paremal>
2020-08-19 18:18:00 +0530

    splitting text file to words and count unique words

--- src/com/paremal/lamda/operations/PrimitivStreamOperations.java
+++ src/com/paremal/lamda/operations/PrimitivStreamOperations.java
@@ -1,16 +1,19 @@
 package com.paremal.lamda.operations;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class PrimitivStreamOperations {
 	public static void main(String[] args) {
-		
-		
-		
-		long t= System.nanoTime();
+
+		long t = System.nanoTime();
 
 		Trader raoul = new Trader("Raoul", "Cambridge");
 		Trader mario = new Trader("Mario", "Milan");
@@ -30,41 +33,68 @@ public class PrimitivStreamOperations {
 		 * generating a range of values using intStream 1-100 even numbers
 		 */
 		System.out.println("1-100 even numbers");
-		IntStream.rangeClosed(1, 100).filter(i -> i % 2 == 0).forEach(n-> System.out.printf("%d ", n));
-		
+		IntStream.rangeClosed(1, 100).filter(i -> i % 2 == 0).forEach(n -> System.out.printf("%d ", n));
+
 		/**
 		 * Pythagorean triple
 		 */
 		System.out.println("\nPythagorean triple numbers ");
-		
-		int UPPER_LMIT=1000;
-		long NUMBER_OF_ITEM_TO_DISPLAY=10;
-		long t2=System.nanoTime();
-		Stream<int[]> pythagoreanTriple=IntStream.rangeClosed(1, UPPER_LMIT).boxed()
+
+		int UPPER_LMIT = 1000;
+		long NUMBER_OF_ITEM_TO_DISPLAY = 10;
+		long t2 = System.nanoTime();
+		Stream<int[]> pythagoreanTriple = IntStream.rangeClosed(1, UPPER_LMIT).boxed()
 				.flatMap(a -> IntStream.rangeClosed(a, UPPER_LMIT).filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
-						.mapToObj(b -> new int[] { a, b,(int) Math.sqrt(a*a + b * b )}));
-		long t3=System.nanoTime();
-		
-		
-		
-		pythagoreanTriple.limit(NUMBER_OF_ITEM_TO_DISPLAY).forEach(n-> System.out.println(n[0]+" "+ n[1]+" " +n[2]));
-				
-		System.out.println("time taken"+(t3-t2));
-		
+						.mapToObj(b -> new int[] { a, b, (int) Math.sqrt(a * a + b * b) }));
+		long t3 = System.nanoTime();
+
+		pythagoreanTriple.limit(NUMBER_OF_ITEM_TO_DISPLAY)
+				.forEach(n -> System.out.println(n[0] + " " + n[1] + " " + n[2]));
+
+		System.out.println("time taken" + (t3 - t2));
+
 		/*
 		 * Static method of Stream.of() is used to create stream from values
 		 */
-		
+
 		Stream<String> stream = Stream.of("Java 8 ", "Lambdas ", "springBoot ", "Microservice");
 		stream.map(String::toUpperCase).forEach(System.out::println);
-		
+
 		/*
 		 * Arrays.stream() method used to create stream from array
 		 */
-		int [] values= {11,7,9,3,6,4,10,5,2,8,1};
-		System.out.println("Sum of values is:"+Arrays.stream(values).sum());
+		int[] values = { 11, 7, 9, 3, 6, 4, 10, 5, 2, 8, 1 };
+		System.out.println("Sum of values is:" + Arrays.stream(values).sum());
+		long uniqueWordCount=0;
+		/*
+		 * splitting text file to words and count unique words
+		 */
+		long wordCount=0;
+		try {
+			/*
+			 * split files to word list
+			 */
+			List<String> words=Files.lines(Paths.get("data.txt"), Charset.defaultCharset())
+					.flatMap(line -> Arrays.stream(line.split(" "))).collect(Collectors.toList());
+			/*
+			 * convert word list to unique word list
+			 */
+			
+			List<String> uniqueWords=words.stream().distinct().collect(Collectors.toList());
+			words.stream().forEach(w-> System.out.printf("%s ", w));
+			System.out.println();
+			uniqueWords.stream().forEach(w-> System.out.printf("%s ", w));
+			uniqueWordCount = uniqueWords.stream().count();
+			wordCount=words.stream().count();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
+
+		System.out.printf("\nThere are " + uniqueWordCount + " unique words in data.txt\nAnd total wordcount is:"+wordCount  );
+
 	}
 
 }
