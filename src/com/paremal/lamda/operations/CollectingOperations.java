80ac5591725d1b256cf15dd258b0369e66564ca0
Sheebu <Sheebu@host.docker.internal>
2020-09-03 09:47:57 +0530

    collect and Collectors and reduce operations

--- src/com/paremal/lamda/operations/CollectingOperations.java
+++ src/com/paremal/lamda/operations/CollectingOperations.java
@@ -1,5 +1,37 @@
 package com.paremal.lamda.operations;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.paremal.lamda.util.Utils;
+
 public class CollectingOperations {
+	
+	static List<Transaction> transactions= Utils.getTransactions();
+	
+	public static void main(String[] args) {
+		
+		
+		
+		/*
+		 * grouping operations with  Collectors
+		 */
+		Map<String,List<Transaction>> transactionsByCurrencies=transactions.stream()
+				.collect(Collectors.groupingBy(Transaction::getCurrency));
+		
+		/*
+		 * counting using Stream.cout()
+		 */
+		
+		Long numberOfTransactions=transactions.stream().count();
+		
+		/*
+		 * counting using collect and Collectors
+		 */
+		
+		 numberOfTransactions=transactions.stream().collect(Collectors.counting());
+		
+	}
 
 }
