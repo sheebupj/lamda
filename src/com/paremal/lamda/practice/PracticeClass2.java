aff148585670b37fabf96cd3b5ab08e6a3dfe8d1
sheebu PJ <sheebupj@gmail.com>
2023-04-22 22:35:44 +0530

    print all numbers  with second number is 9 in an array
    print all numbers  with second number is 9 in an array

--- src/com/paremal/lamda/practice/PracticeClass2.java
+++ src/com/paremal/lamda/practice/PracticeClass2.java
@@ -1,9 +1,13 @@
 package com.paremal.lamda.practice;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.paremal.lamda.operations.Employee;
 import com.paremal.lamda.operations.Transaction;
 import com.paremal.lamda.util.Utils;
 
@@ -20,6 +24,23 @@ public class PracticeClass2 {
 		forEach(t->t.stream().filter(t1->t1.getYear()==2012).forEach(System.out::println));
 		//iterate, filter, and print map keys
 		currencyTransactions.entrySet().stream().map(m->m.getKey()).forEach(System.out::println);
+		List<Employee> empList= Arrays.stream(Utils.getEmployees()).collect(Collectors.toList());
+		empList.stream().collect(Collectors.groupingBy(Employee::getDepartment,Collectors.collectingAndThen
+				(Collectors.maxBy(Comparator.comparing(Employee::getSalary)), Optional::get)));
+		
+		/*
+		 * print all numbers with second digit is 9 in a list
+		 */
+		int[] numAr= {16,1347,647,198374,2934};
+		Arrays.stream(numAr).filter(n->{
+			while(n>=100) {
+				n=(n-(n%10))/10;
+			}
+			n=n%10;
+			if(n==9) return true;
+			return false;
+			
+		}).forEach(System.out::println);
 	}
 
 }
