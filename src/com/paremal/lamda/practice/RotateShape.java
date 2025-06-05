f43dee89834e2d73369808f57910d9baedbe2838
Sheebu PJ <sheebupj@gmail.com>
2025-06-05 15:02:47 +0530

    code modified added more comments

--- src/com/paremal/lamda/practice/RotateShape.java
+++ src/com/paremal/lamda/practice/RotateShape.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class RotateSgape {
+public class RotateShape {
 
     /*
     rotate 2dlist of integers (length and breadth equel
@@ -73,16 +73,24 @@ public class RotateSgape {
 
     }
 
+    static List<List<Integer>> rotateAntiClockwise(List<List<Integer>> llist) {
+        int size = llist.size();
+        Integer[][] twoDArr=llist.stream().map(l-> l.stream().toArray(Integer[]::new)).toArray(Integer[][]::new);
+
+        return Stream.of(rotateAntiClockewise(twoDArr)).map(Arrays::asList).collect(Collectors.toList());
+
+
+    }
     /*
     1 2 3
     4 5 6
     7 8 9
-    After rotating anti-clockwise  it become
-    3 6 9
-    2 5 8
-    1 4 7
+    after rotating clockwise
+    7 4 1
+    8 5 2
+    9 6 3
      */
-    static Integer [][] rotateClockewise( Integer [][] twodAr){
+  static Integer [][] rotateClockewise( Integer [][] twodAr){
         int size=twodAr.length;
         int indexSize=size-1;
         Integer[][] resultArr= new Integer[size][size];
@@ -93,6 +101,38 @@ public class RotateSgape {
         }
         return  resultArr;
     }
+
+
+    /*
+   1 2 3
+   4 5 6
+   7 8 9
+   After rotating anti-clockwise  it become
+   3 6 9
+   2 5 8
+   1 4 7
+    */
+    static Integer [][] rotateAntiClockewise( Integer [][] twodAr){
+        int size=twodAr.length;
+        int indexSize=size-1;
+        Integer[][] resultArr= new Integer[size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                resultArr[i][j]=twodAr[j][indexSize-i];
+            }
+        }
+        return  resultArr;
+    }
+
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+    after vertical flip
+    7 8 9
+    4 5 6
+    1 2 3
+     */
     static Integer [][] flipVertical(Integer [][] twodAr){
         int size=twodAr.length;
         int indexSize=size-1;
@@ -104,6 +144,16 @@ public class RotateSgape {
         }
         return  resultArr;
     }
+
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+    after horizontal flip
+    3 2 1
+    6 5 4
+    9 8 7
+     */
     static Integer [][] flipHorzontal(Integer [][] twodAr){
         int size=twodAr.length;
         int indexSize=size-1;
@@ -118,6 +168,8 @@ public class RotateSgape {
 
 
     public static void main(String[] args) {
+        System.out.println("Enter integers n*n (squire) number of numbers in one line equal to number of line and after enter additional Enter key");
+        System.out.println(":");
        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         List<List<Integer>> input = new ArrayList<>();
         String line=null;
@@ -135,14 +187,32 @@ public class RotateSgape {
             }
         }
         display(input);
+
         List<List<Integer>> resul1 = rotateClockwise(input);
-        List<List<Integer>> result2= flipVertical(input);
-        List<List<Integer>> result3= flipHorizontal(input);
+        System.out.println("after rotating clockwise");
         display(resul1);
+        System.out.println();
+        display(input);
+        List<List<Integer>> resul4 = rotateAntiClockwise(input);
+        System.out.println("after rotating anticlockwise");
+        display(resul4);
+        System.out.println();
+
+        display(input);
+        System.out.println("after vertical flip");
+        List<List<Integer>> result2= flipVertical(input);
         display(result2);
+        System.out.println();
+        display(input);
+        System.out.println("after horizontal flip");
+        List<List<Integer>> result3= flipHorizontal(input);
         display(result3);
 
     }
+
+    /*
+     display data from two-dimensional list
+     */
     static void display(List<List<Integer>> inputList) {
         for (List<Integer> olist : inputList) {
             for (Integer igr : olist) {
@@ -150,10 +220,14 @@ public class RotateSgape {
             }
             System.out.println();
         }
-        System.out.println();
+
     }
+
+    /*
+     display data from two-dimensional Array
+     */
     static void displayArray(Integer[][] ar2d) {
-        List<List<Integer>> inputList = Arrays.stream(ar2d).map(Arrays::asList).collect(Collectors.toList());
+        List<List<Integer>> inputList = Arrays.stream(ar2d).map(Arrays::asList).toList();
         display(inputList);
     }
 }
